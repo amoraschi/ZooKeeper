cc2420f6d8fba30b94d2120a0d02e6028e480921
Peletic <its.peletic@gmail.com>
2023-04-10 12:10:55 -0700

    wait i forgor .length

--- src/commands/recountrole.ts
+++ src/commands/recountrole.ts
@@ -3,9 +3,8 @@ export default {
     description: 'Observes all users with Monki role and if they lack history in DB then adds them.',
     options: [],
     execute: async (interaction: any) => {
-
         await interaction.reply({
-            content: `\`${interaction.guild.roles.get(process.env.MONKI_ROLE_ID).members}\` users with monkey role.`,
+            content: `\`${interaction.guild.roles.get(process.env.MONKI_ROLE_ID).members.length}\` users with monkey role.`,
             ephemeral: true
         })
     }
